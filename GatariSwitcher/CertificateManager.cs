55287336607fcf8281bcd6fd94647367fa26b312
nyawk <kotmrrr@protonmail.com>
2017-02-10 01:43:50 +0300

    Initial commit

---
+++ GatariSwitcher/CertificateManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GatariSwitcher
+{
+    class CertificateManager
+    {
+        public bool Status()
+        {
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2Collection c = store.Certificates.Find(X509FindType.FindBySubjectName, "*.ppy.sh", true);
+            bool result = (c.Count > 0);
+            store.Close();
+
+            return result;
+        }
+
+        public void Install()
+        {
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2 certificate = new X509Certificate2(GatariSwitcher.Properties.Resources.gatari);
+            store.Add(certificate);
+            store.Close();
+        }
+
+        public void Uninstall()
+        {
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2Collection certs = store.Certificates.Find(X509FindType.FindBySubjectName, "*.ppy.sh", true);
+            try
+            {
+                foreach (var c in certs)
+                {
+                    store.Remove(c);
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+    }
+}
---
+++ GatariSwitcher/Controls/KawaiiButton.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GatariSwitcher.Controls
+{
+    public partial class KawaiiButton : UserControl
+    {
+        public KawaiiButton()
+        {
+            InitializeComponent();
+        }
+
+        static DependencyProperty CaptionProperty = DependencyProperty.Register("Caption", typeof(object), typeof(KawaiiButton));
+
+        public object Caption
+        {
+            get
+            {
+                return GetValue(CaptionProperty);
+            }
+            set
+            {
+                SetValue(CaptionProperty, value);
+            }
+        }
+
+
+        private void layoutGrid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            backgroundRectangle.Fill = new SolidColorBrush(Color.FromArgb(0xFF, 0x1A, 0x1A, 0x1A));
+            textLabel.Foreground = Brushes.White;
+        }
+
+        private void layoutGrid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            backgroundRectangle.Fill = Brushes.White;
+            textLabel.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0x1A, 0x1A, 0x1A));
+        }
+    }
+}
