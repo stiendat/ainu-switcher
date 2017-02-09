ce93d42ebe05f18e595074496dafa490436beb6a
nyawk <kotmrrr@protonmail.com>
2017-02-10 01:43:50 +0300

    Initial commit

---
+++ GatariSwitcher/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GatariSwitcher
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ServerSwitcher switcher = new ServerSwitcher();
+            modeButton.Caption = (switcher.GetCurrentServer()) ? "Перейти на оф с чертями" : "Перейти на гатари с кентами";
+            titleLabel.Content = (switcher.GetCurrentServer()) ? "Вы играете на гатари с кентами" : "Вы играете на офе с чертями";
+
+            CertificateManager cert = new CertificateManager();
+            certificateButton.Caption = (cert.Status()) ? "Удалить сертификат" : "Установить сертификат";
+        }
+
+        private void titleRectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void titleLabel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void closeButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void urlBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"https://osu.gatari.pw");
+        }
+
+        private void modeButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ServerSwitcher switcher = new ServerSwitcher();
+            if (switcher.GetCurrentServer())
+            {
+                try
+                {
+                    switcher.SwitchToOfficial();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка!\n" + ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    switcher.SwitchToGatari();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка!\n" + ex.Message);
+                }
+            }
+
+            bool status = switcher.GetCurrentServer();
+            modeButton.Caption = (status) ? "Перейти на оф с чертями" : "Перейти на гатари с кентами";
+            titleLabel.Content = (status) ? "Вы играете на гатари с кентами" : "Вы играете на офе с чертями";
+        }
+
+        private void certificateButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            CertificateManager cert = new CertificateManager();
+            if (cert.Status())
+            {
+                try
+                {
+                    cert.Uninstall();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка!\n" + ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    cert.Install();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка!\n" + ex.Message);
+                }
+            }
+            certificateButton.Caption = (cert.Status()) ? "Удалить сертификат" : "Установить сертификат";
+        }
+    }
+}
---
+++ GatariSwitcher/ServerSwitcher.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GatariSwitcher
+{
+    class ServerSwitcher
+    {
+        const string GATARI_ADDRESS = "185.146.170.233";
+
+        public void SwitchToGatari()
+        {
+            string hostsPath = getHostsPath();
+
+            string[] lines = File.ReadAllLines(hostsPath);
+            List<string> result = new List<string>();
+
+            foreach (var i in lines)
+            {
+                if (!i.Contains("ppy.sh"))
+                {
+                    result.Add(i);
+                }
+            }
+
+            result.Add(GATARI_ADDRESS + "   osu.ppy.sh");
+            result.Add(GATARI_ADDRESS + "   c.ppy.sh");
+            result.Add(GATARI_ADDRESS + "   c1.ppy.sh");
+            result.Add(GATARI_ADDRESS + "   a.ppy.sh");
+            result.Add(GATARI_ADDRESS + "   i.ppy.sh");
+
+            File.WriteAllLines(hostsPath, result);
+        }
+
+        public void SwitchToOfficial()
+        {
+            string hostsPath = getHostsPath();
+
+            string[] lines = File.ReadAllLines(hostsPath);
+            List<string> result = new List<string>();
+
+            foreach (var i in lines)
+            {
+                if (!i.Contains("ppy.sh"))
+                {
+                    result.Add(i);
+                }
+            }
+
+            File.WriteAllLines(hostsPath, result); 
+        }
+
+        /// <summary>
+        /// Get current osu! server
+        /// </summary>
+        /// <returns>true - gatari, false - official</returns>
+        public bool GetCurrentServer()
+        {
+            string[] lines = File.ReadAllLines(getHostsPath());
+            foreach (var i in lines)
+            {
+                if (i.Contains("osu.ppy.sh") && !i.Contains("#"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private string getHostsPath()
+        {
+            string windir = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+            string result = Path.Combine(windir, "System32", "drivers", "etc", "hosts");
+
+            return result;
+        }
+    }
+}
